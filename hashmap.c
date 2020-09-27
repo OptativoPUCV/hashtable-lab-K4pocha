84764ac95614f5b1d7b1d8154eb6f74b21d13273
repl.it user <replituser@example.com>
2020-09-27 17:04:17 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -84,8 +84,7 @@ HashMap * createMap(long capacity) {
   map->size = 0;
   int i;
   for (i = 0; i < capacity; i++) {
-    //if (map->buckets != NULL)
-      map->buckets[i]->value = NULL;
+    map->buckets = NULL;
   }
   map->current = 0;
 
